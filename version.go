deb799744c6ed015fd134833c7d4c00f15112f11
Adeola Okunola <adeola.okunola@invoance.com>
2026-07-14 01:22:16 +0300

    chore: release 0.2.0

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package invoance
 
 // SDKVersion is the released version of this SDK. It is embedded in the
 // User-Agent header sent on every request.
-const SDKVersion = "0.1.0"
+const SDKVersion = "0.2.0"
